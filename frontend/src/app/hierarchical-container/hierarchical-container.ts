49e2692c2e802d99a6fdc71ef4b299e09c987a03
wout-van-damme <woutvandamme.wvd@gmail.com>
2026-07-05 19:52:48 +0200

    add node button

--- frontend/src/app/hierarchical-container/hierarchical-container.ts
+++ frontend/src/app/hierarchical-container/hierarchical-container.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ContentNode } from '../content-node/content-node';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hierarchical-container',
   standalone: true,
-  imports: [ContentNode],
+  imports: [ContentNode, FormsModule],
   templateUrl: './hierarchical-container.html',
   styleUrl: './hierarchical-container.scss'
 })
 export class HierarchicalContainer {
+  showModal = false;
+
+  selectedType = 'project';
+  title = '';
+  description = '';
+
+  openModal(): void {
+    this.selectedType = 'project';
+    this.title = '';
+    this.description = '';
+    this.showModal = true;
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+  }
+
+  onSubmit(): void {
+    console.log({ type: this.selectedType, title: this.title, description: this.description });
+    this.closeModal();
+  }
 }
