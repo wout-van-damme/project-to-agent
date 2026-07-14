2b51c11ead8e4feff5e4a91560646aa82aaf0f79
wout-van-damme <woutvandamme.wvd@gmail.com>
2026-07-14 21:40:31 +0200

    render node details

--- frontend/src/app/node-detail/node-detail.ts
+++ frontend/src/app/node-detail/node-detail.ts
@@ -1,13 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { marked } from 'marked';
 import { Node } from '../content-node/content-node';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-node-detail',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './node-detail.html',
   styleUrl: './node-detail.scss'
 })
@@ -15,18 +19,24 @@ export class NodeDetail implements OnInit {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private http = inject(HttpClient);
+  private sanitizer = inject(DomSanitizer);
 
-  node: Node | null = null;
+
+  node$: Observable<Node> | undefined;
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
-      this.http.get<Node>(`${environment.backendUrl}/node/getNode/${id}`)
-        .subscribe((data) => this.node = data);
+      this.node$ = this.http.get<Node>(`${environment.backendUrl}/node/getNode/${id}`);
     }
   }
 
   goBack(): void {
     this.router.navigate(['/']);
+  }
+
+  renderMarkdown(text: string): SafeHtml {
+    const html = marked.parse(text) as string;
+    return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 }
